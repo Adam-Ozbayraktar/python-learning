ffcaa1361062a223af3b24a234936639adf88920
AdamOz <adamozbayraktar@live.com>
2019-09-10 10:48:05 +0200

    Adding to database and stylesheet not working.

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 # configuration
 DATABASE = 'blog.db'
@@ -16,6 +17,16 @@ app.config.from_object(__name__)
 # function used for connection to the database
 def connection_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -37,9 +48,33 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connection_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)',
+            [request.form['title'], request.form['posts']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
 @app.route('/main')
+# This is called a decorator, main() is wrapped in loggin required then when the user
+# logs in main function is initiated.
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connection_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
